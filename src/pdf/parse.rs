888acbc0ec7e3b541fde18494a66e65d8673ba61
alockinalock <kanjigawa@gmail.com>
2024-04-16 23:47:59 -0500

    deprecated old parser

--- src/pdf/parse.rs
+++ src/pdf/parse.rs
@@ -1,7 +1,9 @@
 use regex::Regex;
 use std::collections::HashMap;
 
+
 // Returns an unsorted HashMap containing the word (string) and number of instances (usize).
+#[deprecated = "The function better_parse_for_words has a better word detection algorithim. It is recommended to use that function instead of this one."]
 pub fn parse_for_words(data: &String) -> HashMap<String, usize> {
     // make the string all lowercase for better word detection
     let fixed_data = data.to_ascii_lowercase();
@@ -17,19 +19,24 @@ pub fn parse_for_words(data: &String) -> HashMap<String, usize> {
     word_counts
 }
 
-// TODO: fix regex
+// Uses regex and splits by whitespace to find words.
+// Returns an unsorted HashMap containing the word (string) and number of isntances (usize)
 pub fn better_parse_for_words(data: &str) -> HashMap<String, usize> {
     let fixed_data = data.to_ascii_lowercase();
-    let re = Regex::new(r"[^\w-]+").unwrap(); // Matches any character that is not a word character or hyphen
-    let cleaned_data = re.replace_all(&fixed_data, " "); // Replace punctuation with whitespace
+    let re = Regex::new(r"[^\w-]+").unwrap(); 
+    let cleaned_data = re.replace_all(&fixed_data, " "); 
+
     let mut word_counts: HashMap<String, usize> = HashMap::new();
+
     let words: Vec<&str> = cleaned_data.split_whitespace().collect();
+
     for word in words {
         *word_counts.entry(word.to_string()).or_insert(0) += 1;
     }
     word_counts
 }
 
+// Returns a sorted vector from most instances to least instances for the words.
 pub fn sort_by_instances(data: HashMap<String, usize>) -> Vec<(String, usize)> {
     let mut sorted_entries: Vec<(String, usize)> = data.into_iter().collect();
     sorted_entries.sort_by(|a, b| b.1.cmp(&a.1));
